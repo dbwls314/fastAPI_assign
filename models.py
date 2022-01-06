6ec06453b4f40e973a804f5f29788370455dfc0d
이유진 <youjin@iyujin-ui-MacBookAir.local>
2022-01-06 14:52:46 +0900

    email unique 추가

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
+from sqlalchemy import UniqueConstraint
 
 class UserBase(SQLModel):
     id: int
@@ -8,11 +9,12 @@ class UserBase(SQLModel):
 
 class User(UserBase, table=True):
     __tablename__ = 'users'
+    __table_args__ = (UniqueConstraint("email"),)
     id : Optional[int] = Field(default = None, primary_key=True)
-    email : str = Field(regex = "[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", description="email error") 
-    password : str = Field(nullable=False, max_length = 300, regex = "(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}", description="password error") 
+    email : str = Field(regex = "[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+") 
+    password : str = Field(nullable=False, max_length = 300, regex = "(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}") 
 
-class UserResponse(UserBase):    
+class UserResponse(UserBase):  
     id: str
 
 class UserUpdate(SQLModel):
@@ -33,23 +35,3 @@ class Trash(SQLModel, table=True):
     user_id : int = Field(default = None, foreign_key = 'users.id')
     account_id : int = Field(default = None, foreign_key = 'accounts.id')
 
-# class AccountBase(SQLModel):
-#     id: int
-#     price: int
-#     memo: str
-
-# class Account(AccountBase, table=True):
-#     __tablename__ = 'accounts'
-#     id : Optional[int] = Field(default = None, primary_key=True)
-#     price : int 
-#     memo : str
-#     user_id : int = Field(default = None, foreign_key = 'users.id')
-
-# class TrashBase(SQLModel):
-#     id: int
-
-# class Trash(TrashBase, table=True):
-#     __talename__ = 'trashs'
-#     id : Optional[int] = Field(default = None, primary_key=True)
-#     user_id : int = Field(default = None, foreign_key = 'users.id')
-#     account_id : int = Field(default = None, foreign_key = 'accounts.id')
