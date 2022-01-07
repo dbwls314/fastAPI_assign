97923cb439f7b339e1186ed6a1ec215055469035
이유진 <youjin@iyujin-ui-MacBookAir.local>
2022-01-07 14:45:41 +0900

    review 반영

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 import bcrypt
 from fastapi.exceptions import HTTPException
 from fastapi import FastAPI
-from typing import List
-from sqlmodel import Session, create_engine, select
+from sqlmodel import Session, select
 from starlette.middleware.cors import CORSMiddleware
 
 from db import engine, create_db_and_tables
-from models.models import User, UserResponse, UserUpdate
+from .models.models import User, UserUpdate
 
 app = FastAPI()
 
@@ -16,7 +15,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)    
+)
 
 @app.post("/users")
 def create_user(user:User): 
@@ -29,12 +28,6 @@ def create_user(user:User):
         session.commit()
         return {"message" : "create user"}
 
-@app.get("/users", response_model=List[UserResponse])
-def get_users():
-    with Session(engine) as session:
-        users = session.exec(select(User)).all()  
-        return users
-
 @app.get("/users/{user_id}")
 def get_user(user_id : int):
     with Session(engine) as session:
@@ -43,21 +36,20 @@ def get_user(user_id : int):
             raise HTTPException(status_code = 404, detail = "user not exist")
         return {"result" : user}
 
-@app.patch("/update/users/{user_id}")
+@app.patch("/users/{user_id}")
 def update_user(user_id : int, user:UserUpdate):
     with Session(engine) as session:
         db_user = session.exec(select(User).where(User.id == user_id)).one()
+        
+        if not db_user:
+            raise HTTPException(status_code = 404, detail = "user not exist")
 
-        if not user.email:
-            db_user.email = db_user.email
-        else :
+        if user.email:
             db_user.email = user.email
 
-        if not user.password:
-            db_user.password = db_user.password
-        else:
+        if user.password:
             db_user.password = user.password
-                       
+
         session.add(db_user) 
         session.commit()
         session.refresh(db_user)
@@ -68,11 +60,11 @@ def delete_user(user_id : int):
     with Session(engine) as session:
         user = session.get(User, user_id)
         if not user:
-            raise HTTPException(status_code = 404, detail = "user not exist")
+            raise HTTPException(status_code = 204, detail = "no content")
         else:
             session.delete(user)
             session.commit()
-        return {"message" : "user delete"}
+        return {}
 
 @app.on_event("startup")
 def startup_event():
--- app/models/models.py
+++ app/models/models.py
@@ -34,4 +34,3 @@ class Trash(SQLModel, table=True):
     id : Optional[int] = Field(default = None, primary_key=True)
     user_id : int = Field(default = None, foreign_key = 'users.id')
     account_id : int = Field(default = None, foreign_key = 'accounts.id')
-
